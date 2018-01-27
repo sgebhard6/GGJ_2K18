4d917a3b916b7aa46352939d40aa57853410870d
sgebhard <gebhas6@gmail.com>
2018-01-26 22:07:11 -0600

    got the line renderer working

--- Assets/Scripts/LightTransmitter.cs
+++ Assets/Scripts/LightTransmitter.cs
@@ -12,7 +12,8 @@ public class LightTransmitter : MonoBehaviour {
     public LayerMask layerMask;
 
     int currentRayCount = 0;
-    List<Ray> rayList = new List<Ray>();
+    //List<Ray> rayList = new List<Ray>();
+    List<LightBeam> rayList = new List<LightBeam>();
 
     Vector3 newDir;
 
@@ -20,13 +21,20 @@ public class LightTransmitter : MonoBehaviour {
 
     private void Start()
     {
+        Reset();
+    }
+
+    private void Reset()
+    {
         lightRay = new Ray(lightObject.transform.position, lightObject.transform.right);
-        lightRayRenderer.positionCount = maxRays;
+        currentRayCount = 0;
+        rayList.Clear();
     }
 
     void Update () {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            Reset();
             TransmitLight(lightRay);
         }
 
@@ -40,14 +48,18 @@ public class LightTransmitter : MonoBehaviour {
             currentRayCount++;
             
             RaycastHit2D hit = Physics2D.Raycast(_ray.origin, _ray.direction, maxDistance, layerMask);
-            rayList.Add(lightRay);
+            //rayList.Add(lightRay);
+            rayList.Add(new LightBeam(lightRay, hit.distance));
 
             if (hit.collider != null)
             {
-                newDir = Vector3.Reflect(_ray.direction, hit.normal);
-                lightRay.origin = hit.point + hit.normal * 0.01f;
-                lightRay.direction = newDir;
-                TransmitLight(lightRay);
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Reflective"))
+                {
+                    newDir = Vector3.Reflect(_ray.direction, hit.normal);
+                    lightRay.origin = hit.point + hit.normal * 0.01f;
+                    lightRay.direction = newDir;
+                    TransmitLight(lightRay);
+                }
             }
         }
     }
@@ -55,13 +67,32 @@ public class LightTransmitter : MonoBehaviour {
     void DrawRays()
     {
         if (rayList.Count < 1) return;
-        Vector3[] points = new Vector3[maxRays];
-        for(int i = 0; i < rayList.Count; i++)
+        lightRayRenderer.positionCount = rayList.Count + 1;
+        Vector3[] points = new Vector3[rayList.Count + 1];
+        for(int i = 0; i < points.Length; i++)
         {
-            points[i] = rayList[i].origin;
-            //Debug.DrawRay(rayList[i].beam.origin, rayList[i].beam.direction * 100, Color.green, 1f);
+            if (i < rayList.Count)
+            {
+                points[i] = rayList[i].beam.origin;
+                //Debug.DrawRay(rayList[i].beam.origin, rayList[i].beam.direction * 20, Color.green);
+            }
+            else
+                points[i] = rayList[i - 1].beam.origin + (rayList[i - 1].beam.direction * rayList[i - 1].distance);
+            
         }
 
         lightRayRenderer.SetPositions(points);
+    }
+}
+
+public class LightBeam
+{
+    public Ray beam;
+    public float distance;
+
+    public LightBeam(Ray _beam, float _distance)
+    {
+        beam = _beam;
+        distance = _distance;
     }
 }
