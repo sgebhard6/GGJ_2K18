fbba2c990dbca20c6b67412acc6006ebd60d7380
gebharsj <gebharsj@gmatc.matc.edu>
2018-01-28 04:12:48 -0600

    changed borders to continually update positions

--- Assets/Scripts/Utility/PlaceBorderWall.cs
+++ Assets/Scripts/Utility/PlaceBorderWall.cs
@@ -14,21 +14,33 @@ public class PlaceBorderWall : MonoBehaviour
 
 	public Border border;
 
+	Camera mainCam;
+
 	void Start ()
+	{
+		mainCam = Camera.main;
+	}
+
+	void LateUpdate ()
+	{
+		SetBorder ();
+	}
+
+	void SetBorder ()
 	{
 		Vector3 newPos = new Vector3 ();
 		switch (border) {
 		case Border.Top:
-			newPos = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width * 0.5f, Screen.height, 0));
+			newPos = mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 0.5f, Screen.height, 0));
 			break;
 		case Border.Bottom:
-			newPos = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width * 0.5f, 0, 0));
+			newPos = mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 0.5f, 0, 0));
 			break;
 		case Border.Left:
-			newPos = Camera.main.ScreenToWorldPoint (new Vector3 (0, Screen.height * 0.5f, 0));
+			newPos = mainCam.ScreenToWorldPoint (new Vector3 (0, Screen.height * 0.5f, 0));
 			break;
 		case Border.Right:
-			newPos = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height * 0.5f, 0));
+			newPos = mainCam.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height * 0.5f, 0));
 			break;
 		}
 
