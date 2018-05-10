b391e26e71d83a94df0bd1f20c4ad956eb699dd0
sgebhard6 <gebhas6@gmail.com>
2018-05-09 21:25:10 -0500

    added automated build script

---
+++ Assets/Scripts/Editor/AutomatedBuildProcess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AutomatedBuildProcess {
+
+    private static string windowsBuildFolderPath = "D:/C_Drive_Stuff/Users/gebha/GameDev/GompBot_Build";
+
+    public static void StartBuild()
+    {
+        List<string> enabledScenePathNames = new List<string>();
+        foreach(var buildSettingsScene in EditorBuildSettings.scenes)
+        {
+            if(buildSettingsScene.enabled)
+                enabledScenePathNames.Add(buildSettingsScene.path);
+        }
+
+        string ExecutableDirectoryPath = windowsBuildFolderPath + "GompBot_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + "/";
+
+        if(!Directory.Exists(ExecutableDirectoryPath))
+        {
+            Directory.CreateDirectory(ExecutableDirectoryPath);
+        }
+
+        string windowsExecutableName = "GompBot_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".exe";
+
+        Debug.Log("Starting to make Windows Build");
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = enabledScenePathNames.ToArray();
+        buildPlayerOptions.locationPathName = ExecutableDirectoryPath + windowsExecutableName;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
